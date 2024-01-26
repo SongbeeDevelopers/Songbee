c044ad4a6f4772b5d7a743e235547bf209e2781d
Abubakar Aden <abubakar.aden978@gmail.com>
2024-01-26 11:12:40 -0600

    Formatted ?

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -5,8 +5,10 @@ import "./LandingPage.css";
 function HeroSection() {
   return (
     <div className="hero">
-      <h1>Custom Songs <br /> For <span className="every">Every</span> Moment</h1>
-      <img src="/song.png"/>
+      <h1>
+        Custom Songs <br /> For <span className="every">Every</span> Moment
+      </h1>
+      <img src="/song.png" />
       <p>Studio-Quality Custom Songs For Any Occasion</p>
       <button>Start Your Song</button>
     </div>
