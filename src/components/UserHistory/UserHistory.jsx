0f3d43c1614ecba17ed2e6786d57efb3ac972df2
Brian Werner <brianmw199@gmail.com>
2024-01-26 14:32:58 -0600

    Created the userHistory page

---
+++ src/components/UserHistory/UserHistory.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+// This function will display the user's order history
+function UserHistory() {
+    const dispatch = useDispatch();
+    // const request = useSelector((store) => store.)
+
+// Will useEffect and dispatch t call to the store
+
+// will need to get the user's song request
+    return (
+        <p>Status</p>
+    )
+
+}
+
+export default UserHistory;
