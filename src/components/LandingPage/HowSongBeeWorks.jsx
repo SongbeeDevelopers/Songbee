d80d8f9aea4e44a23368d0a32384ff0fd3b9cc05
Abubakar Aden <abubakar.aden978@gmail.com>
2024-01-26 11:11:52 -0600

    created new component

---
+++ src/components/LandingPage/HowSongBeeWorks.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./LandingPage.css";
+
+function HowSongBeeWorks() {
+  return (
+    <div className="works">
+    <h1>How Song Bee Works</h1>
+    </div>
+  );
+}
+
+export default HowSongBeeWorks;
