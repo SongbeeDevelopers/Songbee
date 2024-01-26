e9f44eafb368d940e3be17d4f030975038ef61e0
Leo Johnson <leochinonejohnson@gmail.com>
2024-01-26 14:33:07 -0600

    set up order layout

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 import './OrderPage.css'
 
+
 function OrderPage() {
 
   const history = useHistory()
-
+  
   const submitOrder = () => {
     history.push('/checkout')
   }
@@ -21,19 +23,19 @@ function OrderPage() {
       <p className='orderSubHeader'>Add your song to checkout, then fill out your song detail form!</p>
 
       <form className='orderForm'>
-        <select className='orderInput' name="delivery">
-          <option selected disabled value={null}>Select Delivery Option</option>
+        <select className='orderInput' name="delivery_days">
+          <option selected disabled>Select Delivery Option</option>
           <option value="3">3 Day Delivery</option>
           <option value="4">4 Day Delivery</option>
           <option value="6">Standard 6 Day Delivery</option>
         </select>
         <select className='orderInput' name="streaming">
-          <option selected disabled value={null}>Select Streaming Option</option>
+          <option selected disabled>Select Streaming Option</option>
           <option value={true}>Add Streaming</option>
           <option value={false}>No Streaming</option>
         </select>
-        <select className='orderInput' name="verses">
-          <option selected disabled value={null}>Select Number of Verses</option>
+        <select className='orderInput' name="extra_verse">
+          <option selected disabled>Select Number of Verses</option>
           <option value="2">Standard 2 Verses</option>
           <option value="3">Add Extra Verse</option>
         </select>
