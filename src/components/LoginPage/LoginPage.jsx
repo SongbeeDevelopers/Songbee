970a5d7de227dbe26e56c5119ec7f921ff903057
Leo Johnson <leochinonejohnson@gmail.com>
2024-01-29 09:42:01 -0600

    finished login style

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
 
+import './LoginPage.css'
+
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div>
+    <div className='loginContainer'>
+
+      <h1 className='loginHeader'>Songbee Account Login</h1>
+
       <LoginForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
+      <button
+        type="button"
+        className="regButton"
+        onClick={() => {history.push('/registration')}}
+      >
+        Register
+      </button>
+
     </div>
   );
 }
