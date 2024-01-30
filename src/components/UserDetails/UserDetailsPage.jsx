6b8ccab5e8de7d653b2f92b04612d3f433646754
Brian Werner <brianmw199@gmail.com>
2024-01-30 10:53:16 -0600

    Setting up the user details page with a player

--- src/components/UserDetails/UserDetailsPage.jsx
+++ src/components/UserDetails/UserDetailsPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 // This function will display the user's song details with a player so they can review 
 // their song
 
 function UserDetails() {
     const dispatch = useDispatch();
-    
+    const ID = useParams();
+
+    return (
+        <div className='audioPlayer'>
+           <audio controls src="https://res.cloudinary.com/dcram3k0q1/video/upload/v1705711050/syquoqbokasssbuem1sf.wav"></audio>  
+
+        </div>
+    )
+
 
 }
 
--- src/components/UserHistory/UserHistory.jsx
+++ src/components/UserHistory/UserHistory.jsx
@@ -17,8 +17,8 @@ useEffect(() => {
     dispatch({type: 'FETCH_USER_REQUESTS' })
 },[]);
 
-const handleSongRequest = () => {
-  history.push('/details')
+const handleSongRequest = (id) => {
+  history.push(`/details/${id}`)
 }
 
 // will need to get the user's song request
@@ -27,15 +27,19 @@ const handleSongRequest = () => {
       <h2>Here is your status {user.username}</h2>
       
       {/* //Map over the request to display */}
-      <div>
-        {userRequests.map((request, i) => (
-           <p key={i}> Your Song {new Date(request.created_at).toLocaleString('en-us')}</p> 
-              
-        ))}
-        {/* // The handle click will bring the user to the details page  where they can listen to their song
+      {/* // The handle click will bring the user to the details page where they can listen to their song
           // Also will have the ability purchase add ons
           */}
-          <button className='detailsBtn' onClick={ handleSongRequest}>Details</button>
+      <div>
+        {userRequests.map((request, i) => (
+          <>
+           <p key={i}> Your Song {new Date(request.created_at).toLocaleString('en-us')}</p> 
+           <button className='detailsBtn' onClick={() => handleSongRequest(request.id)}>Details</button>
+           </>
+              
+        ))}
+        
+          
       </div>
      
       </> 
