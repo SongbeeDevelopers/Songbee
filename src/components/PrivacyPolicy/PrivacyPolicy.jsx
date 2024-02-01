3042929ad03aa51047730b2cf9743bd2c573aa3f
Abubakar Aden <abubakar.aden978@gmail.com>
2024-02-01 14:16:29 -0600

    started working on this ...

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function PrivacyPolicy() {
   return (
     <div className="container">
-      <p>Privacy Policy</p>
+      <h1>Privacy Policy</h1>
+      <p>
+        Protecting your private information is our priority. This Statement of
+        Privacy applies to www.songbee.com, and Songbee, LLC and governsdata
+        collection and usage. For the purposes of this Privacy Policy, unless
+        otherwise noted, all references to Songbee, LLC include www.songbee.com,
+        Songbee and www.songbee.com. The Songbee website is a We are an
+        ecommerce website that sells custom songs for any and every occasion
+        including birthdays, weddings, memorials, etc. We also create learning
+        songs that help children meet their learning milestones. site. By using
+        the Songbee website, you consent to the data practices described in this
+        statement.
+      </p>
+
+      <h2>Collection of yourPersonal Information</h2>
+      <p>In order to better provide you with products and services offered, Songbee may collect personally identifiable information, such as your:   
+</p>
     </div>
   );
 }
