b92b47c7439d6a9a7bbc99f961fb3127bf070d39
Abubakar Aden <abubakar.aden978@gmail.com>
2024-02-06 12:15:12 -0600

    created artist post

--- server/routes/artist.router.js
+++ server/routes/artist.router.js
@@ -1,19 +1,61 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
+
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // GET route code here
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+
+router.post("/", rejectUnauthenticated, (req, res) => {
+  const newArtist = req.body;
+  const queryText = `INSERT INTO "artist"
+ ("artist_name","user_id", "vocal_type", "first_name", "last_name")
+  VALUES
+  ($1, $2, $3, $4, $5) returning "id"; `;
+  const queryGenre = `INSERT INTO "artist_genres"
+  ("artist_id", "genre_id")
+  VALUES
+  ($1, $2);`;
+  pool
+    .query(queryText, [
+      newArtist.artist_name,
+      // newArtist.name,
+      req.user.id, // access the id of the current logged in user
+      newArtist.vocal_type,
+      newArtist.first_name,
+      newArtist.last_name,
+    ])
+    .then((result) => {
+      console.log(result.rows);
+      pool
+        .query(queryGenre, [
+          result.rows[0].id, // access the id of the created artist
+          newArtist.genre_id,
+        ])
+        .then((result) => {
+          console.log(result.rows);
+          res.sendStatus(201);
+        })
+        .catch((err) => {
+          console.log("Genre creation failed: ", err);
+          res.sendStatus(500);
+        });
+    })
+    .catch((err) => {
+      console.log("Artist registration failed: ", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
