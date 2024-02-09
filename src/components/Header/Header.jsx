284f7a32ea9ad0c508a4f8882585bf266344da4e
Walker Neudorff <walkerneudorff@gmail.com>
2024-02-09 14:41:24 -0600

    Changed dropdown menu, altered header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-import { styled } from "@mui/material/styles";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
 
 import "./Header.css";
 import { Logout } from "@mui/icons-material";
@@ -25,35 +20,30 @@ function Header() {
   const dispatch = useDispatch();
 
   const user = useSelector((store) => store.user);
-
-  // persistent drawer logic
-  const [open, setOpen] = useState(false);
-  const drawerWidth = 240;
-
-  const DrawerHeader = styled("div")(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-start",
-  }));
-
-  // drawer open/close
-  const handleDrawerOpen = () => {
-    setOpen(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-  const handleDrawerClose = () => {
-    setOpen(false);
+  const handleClose = (num) => {
+    setAnchorEl(null);
+    if (num === 1) {
+      history.push("/");
+    } else if (num === 2) {
+      history.push("/order");
+    } else if (num === 3) {
+      history.push("/join-artist");
+    } else if (num === 4) {
+      history.push("/faq");
+    } else if (num === 5) {
+      
+    } else if (num === 6) {
+      history.push("/user");
+    } else if (num === 7) {
+      history.push(`/admin`);
+    }
   };
-  // const scrollToFaq = ()=>{
-  //   const faqSection = document.getElementById("faq")
-  //   if(faqSection){
-  //     faqSection.scrollIntoView({
-  //       behavior:"smooth"
-  //     })
-  //   }
-  // }
+ 
   return (
     <div className="nav">
 
@@ -61,23 +51,6 @@ function Header() {
         <img className="nav-title" src="header-icon.png"></img>
       </Link>
       <div className="nav-right">
-        {/* <span onClick={()=>scrollToFaq()} className="nav-links">
-          Faq
-        </span> */}
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#a5a5a5"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-tally-1"
-        >
-          <path d="M4 4v16" />
-        </svg>{" "} */}
         <Link
           onClick={(e) => {
             if (user?.id) {
@@ -91,172 +64,48 @@ function Header() {
           {user?.id? "Logout" : "Login"}
         </Link>
         <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="end"
-          onClick={handleDrawerOpen}
-          sx={{
-            ...(open && { display: "none" }),
-            // position: "absolute",
-            // right: "2rem",
-            // top: "1rem",
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
-      </div>
-
-      {/* </Toolbar> */}
-
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-          },
-        }}
-        variant="persistent"
-        anchor="right"
-        open={open}
-      >
-        <DrawerHeader
-          sx={{
-            bgcolor: "#feaf17",
-          }}
-        >
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronRightIcon />
+            size="large"
+            edge="start"
+            color="black"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MenuIcon />
           </IconButton>
-          <img className="drawerHeaderBee" src="bee-button.png"></img>
-        </DrawerHeader>
-
-        <Divider />
-
-        <List
-          sx={{
-            bgcolor: "#fff4df",
-          }}
-        >
-          {!user.id && (
-            <ListItem disablePadding>
-              <ListItemButton
-                onClick={() => {
-                  handleDrawerClose();
-                  history.push("/login");
-                }}
-              >
-                <ListItemText primary={"Login"} />
-              </ListItemButton>
-            </ListItem>
-          )}
-
-          {user.id && user.class === 2 && (
-            <ListItemButton
-              onClick={() => {
-                handleDrawerClose();
-                history.push("/artistpage");
-              }}
-            >
-              <ListItemText primary={"Artist Page"} />
-              <div className="navSpacer"></div>
-            </ListItemButton>
-          )}
-
-          {user.id && user.class === 3 && (
-            <ListItemButton
-              onClick={() => {
-                handleDrawerClose();
-                history.push("/admin");
-              }}
-            >
-              <ListItemText primary={"Admin Page"} />
-              <div className="navSpacer"></div>
-            </ListItemButton>
-          )}
-
-          {user.id && (
-            <>
-              <ListItemButton
-                onClick={() => {
-                  handleDrawerClose();
-                  history.push("/user");
-                }}
-              >
-                <ListItemText primary={"User Page"} />
-                <div className="navSpacer"></div>
-              </ListItemButton>
-              <ListItemButton
-                onClick={() => {
-                  handleDrawerClose();
-                  history.push("/join-artist");
-                }}
-              >
-                <ListItemText primary={"Join as Artist"} />
-                <div className="navSpacer"></div>
-              </ListItemButton>
-            </>
-          )}
-
-          <ListItemButton
-            onClick={() => {
-              handleDrawerClose();
-              history.push("/order");
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
             }}
           >
-            <ListItemText primary={"Start a Song"} />
-            <div className="navSpacer"></div>
-          </ListItemButton>
-
-          <ListItemButton
-            onClick={() => {
-              handleDrawerClose();
-              history.push("/artists");
-            }}
-          >
-            <ListItemText primary={"Artists"} />
-            <div className="navSpacer"></div>
-          </ListItemButton>
-
-          <ListItemButton
-            onClick={() => {
-              handleDrawerClose();
-              history.push("/faq");
-            }}
-          >
-            <ListItemText primary={"FAQ"} />
-            <div className="navSpacer"></div>
-          </ListItemButton>
-
-          <ListItemButton
-            onClick={() => {
-              handleDrawerClose();
-              history.push("/about");
-            }}
-          >
-            <ListItemText primary={"About"} />
-            <div className="navSpacer"></div>
-          </ListItemButton>
-
-          {user.id && (
-            <ListItemButton
-              sx={{
-                position: "fixed",
-                bottom: 0,
-                width: drawerWidth,
-                bgcolor: "#feaf17",
-              }}
-              onClick={() => {
-                handleDrawerClose();
-                dispatch({ type: "LOGOUT" });
-              }}
-            >
-              <ListItemText primary={"Logout"} />
-              <div className="navSpacer"></div>
-            </ListItemButton>
-          )}
-        </List>
-      </Drawer>
+            <MenuItem onClick={() => handleClose()}><img className="drawerHeaderBee" src="bee-button.png"></img></MenuItem>
+            <MenuItem onClick={() => handleClose(1)}>Home</MenuItem>
+            {user.id && (
+              <div>
+              <MenuItem onClick={() => handleClose(6)}>My Profile</MenuItem>
+              </div>
+            )}
+            {user.id && user.class === 3 && (
+              <div>
+              <MenuItem onClick={() => handleClose(7)}>Admin Page</MenuItem>
+              </div>
+            )}
+            <MenuItem onClick={() => handleClose(2)}>Start Your Song</MenuItem>
+            <MenuItem onClick={() => handleClose(3)}>Artist Application</MenuItem>
+            <MenuItem onClick={() => handleClose(4)}>FAQ</MenuItem>
+            <MenuItem onClick={() => handleClose(5)}><a href="mailto:hello@songbee.com">Contact Us</a></MenuItem>
+            {/* If a user is logged in, show these links */}
+           
+          </Menu>
+      </div>
     </div>
   );
 }
