c573323f72de26c8748177acaf9a5f77a55ae462
Walker Neudorff <walkerneudorff@gmail.com>
2024-02-12 13:29:39 -0600

    Updated strict mode for presentation

--- src/components/CreationPage/CreationPage.jsx
+++ src/components/CreationPage/CreationPage.jsx
@@ -1,24 +1,25 @@
-import React from "react"
+import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
 function CreationPage() {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const order = useParams();
-    const newOrder = useSelector(store => store.newOrder);
-    console.log('order', order)
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const order = useParams();
+  const newOrder = useSelector((store) => store.newOrder);
+  console.log("order", order);
+  console.log("order.delivery_days", order.delivery_days);
+  if (order.delivery_days) {
     dispatch({
-        type: 'CREATE_SONG_REQUEST',
-        payload: {
-            data: order,
-            history: history
-    }
-    })
-    return (
-        <h1>Loading...</h1>
-    )
+      type: "CREATE_SONG_REQUEST",
+      payload: {
+        data: order,
+        history: history,
+      },
+    });
+  }
+  return <h1>Loading...</h1>;
 }
 
 export default CreationPage;
