69fb607ef055d62bb0c7160c846885f7306d9da3
Leo Johnson <leochinonejohnson@gmail.com>
2024-02-12 14:44:29 -0600

    more conditionals

--- src/components/UserDetails/UserDetailsPage.jsx
+++ src/components/UserDetails/UserDetailsPage.jsx
@@ -110,19 +110,26 @@ function UserDetails() {
               <div className='songDetails'>
                 <h1>To {request.recipient}, from {request.requester}</h1>
 
-                <Typography sx={{ fontSize: 5 }} variant="h2" gutterBottom>
-                  <p className='songTitle'>{request.title}</p> 
-                </Typography>
+                {
+                  request.title && request.artist_name && request.url && request.lyrics ?
+                  <>
+                    <Typography sx={{ fontSize: 5 }} variant="h2" gutterBottom>
+                      <p className='songTitle'>{request.title}</p> 
+                    </Typography>
 
-                <Typography variant="h5" component="div"> 
-                  <p className='artistTitle'> By {request.artist_name}</p>
-                </Typography>
-                {request.url && <audio controls src={request.url} ></audio>}
+                    <Typography variant="h5" component="div"> 
+                      <p className='artistTitle'> By {request.artist_name}</p>
+                    </Typography>
+                    {request.url && <audio controls src={request.url} ></audio>}
 
-                <div>
-                  {request.lyrics && <h2 className='lyricsHeader'>Lyrics:</h2>}
-                  {request.lyrics && <p>{request.lyrics}</p>}
-                </div>
+                    <div>
+                      {request.lyrics && <h2 className='lyricsHeader'>Lyrics:</h2>}
+                      {request.lyrics && <p>{request.lyrics}</p>}
+                    </div>
+                  </>
+                  :
+                  <p>Song in progress!</p>
+                }
               </div>
 
               <div className='detailsContainer'>
@@ -139,28 +146,74 @@ function UserDetails() {
                   </div>
                   <div className='detailsItem2'>
                     <h2>Inspiration:</h2>
-                    <p>{request.inspiration}</p>
+                    {request.inspiration ?
+                      <p>{request.inspiration}</p>
+                      :
+                      <p>No inspiration provided. Sad!</p>
+                    }
                   </div>
                 </div>
 
                 <div className='detailsRow'>
                   <div className='detailsItem'>
                     <h2>Your story:</h2>
-                    <p>{request.story1}</p>
-                    <p>{request.story2}</p>
+                    {
+                      request.story1 ?
+                      <p>{request.story1}</p>
+                      :
+                      <p>First story not provided.</p>
+                    }
+                    {
+                      request.story2 ?
+                      <p>{request.story2}</p>
+                      :
+                      <p>Second story not provided.</p>
+                    }
                   </div>
 
                   <div className='detailsItem2'>
                     <h2>The Importance:</h2>
-                    <p>{request.important_what}<br/>
-                    {request.important_why}</p>
+                    {
+                      request.important_what ?
+                      <p>{request.important_what}</p>
+                      :
+                      <p>What is important was not described.</p>
+                    }
+                    {
+                      request.important_why ?
+                      <p>{request.important_why}</p>
+                      :
+                      <p>Why it's imporant was not described.</p>
+                    }
                   </div>
                 </div>
 
                 <div className='detailsRow'>
                   <div className='detailsItem'>
-                    <h2>Song Details:</h2>
-                    <p>You requested a {request.genre} song with a {request.vocal_type} vocal type, a {request.vibe} vibe, and a {request.tempo} tempo</p>
+                    <h2>Song Parameters:</h2>
+                    {request.genre ?
+                      <p>Genre: {request.genre}</p>
+                      :
+                      <p>No genre provided.</p>
+                    }
+                    {
+                      request.vocal_type ?
+                      <p>Vocal type: {request.vocal_type}</p>
+                      :
+                      <p>No vocal type provided.</p>
+                    }
+                    {
+                      request.vibe ?
+                      <p>Vibe: {request.vibe}</p>
+                      :
+                      <p>No vibe provided.</p>
+                    }
+                    {
+                      request.tempo ?
+                      <p>Tempo: {request.tempo}</p>
+                      :
+                      <p>No tempo provided.</p>
+                    }
                   </div>
                   <div className='detailsItem2'>
                     <h2>Additional Details:</h2>
