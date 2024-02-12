713062a29aa13d23a420b59ff36f6bbd9e5b6ab0
Abubakar Aden <abubakar.aden978@gmail.com>
2024-02-12 10:53:06 -0600

    usertab workk

--- src/components/UserPageTabs/UserPageTabs.jsx
+++ src/components/UserPageTabs/UserPageTabs.jsx
@@ -20,18 +20,14 @@ import Typography from "@mui/material/Typography";
 
 import "./UserPageTabs.css";
 
-
 // Inside here will have the user's email and password and have the option to edit details
-function BasicTabs() {
-
-  const dispatch = useDispatch();
-  const history = useHistory();
-
+export default function BasicTabs() {
+  const [value, setValue] = useState(0);
   const user = useSelector((store) => store.user);
   const emailRef = useRef(user.email);
-
   const passwordRef = useRef("");
-  const [value, setValue] = useState(0);
+  const history = useHistory();
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -73,6 +69,7 @@ function BasicTabs() {
 
   const CustomTabPanel = (props) => {
     const { children, value, index, ...other } = props;
+
     return (
       <div
         role="tabpanel"
@@ -89,7 +86,6 @@ function BasicTabs() {
       </div>
     );
   };
-
   CustomTabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.number.isRequired,
@@ -101,69 +97,56 @@ function BasicTabs() {
       id: `simple-tab-${index}`,
       "aria-controls": `simple-tabpanel-${index}`,
     };
-  };
+  }; // End of tab structure
 
   // This is for the dialog
   const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
 
-
   return (
-    <div className="container">
-    <Box sx={{ height: "80%", borderBottom: 1, borderColor: "divider" }}>
-          <Tabs
-            className="tabHeader"
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <Tab
-              sx={{ color: "orange" }}
-              label="Order History"
-              {...a11yProps(0)}
-            />
-            <Tab 
-              sx={{ color: "orange" }} 
-              label="profile" 
-              {...a11yProps(1)} />
-            <Tab
-              sx={{ color: "orange" }}
-              label="Credit Balance"
-              {...a11yProps(2)}
-            />
-          </Tabs>
-        </Box>
-    <Box sx={{ width: "100%" }}>
-      <Card className="cardBackground" variant="outlined">
-        <CustomTabPanel className="cardBody" value={value} index={0}>
-          <UserHistory />
-        </CustomTabPanel>
+    <>
+      <Box sx={{ height: "80%", borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          className="tabHeader"
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab
+            sx={{ color: "orange" }}
+            label="Order History"
+            {...a11yProps(0)}
+          />
+          <Tab sx={{ color: "orange" }} label="profile" {...a11yProps(1)} />
+          <Tab
+            sx={{ color: "orange" }}
+            label="Credit Balance"
+            {...a11yProps(2)}
+          />
+        </Tabs>
+      </Box>
+      <Box sx={{ width: "100%" }}>
+        <Card className="cardBackground" variant="outlined">
+          <CustomTabPanel className="cardBody" value={value} index={0}>
+            <UserHistory />
+          </CustomTabPanel>
 
-        <CustomTabPanel value={value} index={1}>
-          <h1 className="profileHeader">Personal info</h1>
-          <h3>{user.email}</h3>
+          <CustomTabPanel value={value} index={1}>
+            <h1 className="profileHeader">Personal info</h1>
+            <h3>{user.email}</h3>
 
-          <CardContent variant="outlined">
-            {/* <Typography sx={{ fontSize: 14, mt: 2 }} color="text.secondary" gutterBottom> */}
-            <Button sx={{ color: "black" }} onClick={handleOpen}>
-              Edit Info
-            </Button>
-            <Dialog
-              open={open}
-              keepMounted
-              TransitionComponent={Transition}
-              onClose={handleClose}
-              aria-describedby="alert-dialog-slide-description"
-              sx={{
-                width: 800,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                ml: 20,
-              }}
-            >
-              <Box
+            <CardContent variant="outlined">
+              {/* <Typography sx={{ fontSize: 14, mt: 2 }} color="text.secondary" gutterBottom> */}
+              <Button sx={{ color: "black" }} onClick={handleOpen}>
+                Edit Info
+              </Button>
+              <Dialog
+                open={open}
+                keepMounted
+                TransitionComponent={Transition}
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
                 sx={{
                   width: 800,
                   display: "flex",
@@ -215,21 +198,41 @@ function BasicTabs() {
                     name="password"
                     type="password"
                     size="small"
-                  >
-                    Cancel
-                  </Button>
-                </div>
-              </Box>
-            </Dialog>
-          </CardContent>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={2}>
-          <UserCreditPage />
-        </CustomTabPanel>
-      </Card>
-    </Box>
-    </div>
+                    placeholder="Password"
+                    onChange={(e) => {
+                      passwordRef.current = e.target.value;
+                    }}
+                  />
+
+                  <div className="modalBtns">
+                    <Button
+                      type="submit"
+                      onClick={handleEdit}
+                      variant="contained"
+                      color="success"
+                      size="small"
+                    >
+                      Save
+                    </Button>
+                    <Button
+                      type="submit"
+                      onClick={handleCancel}
+                      variant="contained"
+                      color="secondary"
+                      size="small"
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </Box>
+              </Dialog>
+            </CardContent>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            <UserCreditPage />
+          </CustomTabPanel>
+        </Card>
+      </Box>
+    </>
   );
 }
-
-export default BasicTabs
