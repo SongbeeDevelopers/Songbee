d9f90e71e5d308d1949e1f9d70b6c6151ea22bd9
Leo Johnson <leochinonejohnson@gmail.com>
2024-03-13 14:03:45 -0500

    completed header and get started box

--- src/components/SongbeeJrLanding/HeroSection/HeroSectionJR.jsx
+++ src/components/SongbeeJrLanding/HeroSection/HeroSectionJR.jsx
@@ -4,26 +4,46 @@ import './HeroSectionJR.css';
 
 
 function HeroSectionJR() {
-    return (
-        <>
-        <div className="hero-section">
-            <h1 className="music">Music <span className="forEvery">For <br /> Every</span> Milestone</h1>
-                <img className="underlineImg" src="/underline.png" /> 
-                <p className="pTag">Reach your child's developmental <br />milestones with custom learning songs</p>
-               <div className="image-container">    
-                    <img src="/getStarted.png" className="startSong" /> 
-                    <button className="button">Get Started</button>
-               </div> 
-                <img src="/Bee.png" className="beeImg" />
-                <img src="/bee.gif" /> 
-                <img src="/songbeejr.png" className="overlay-gif"  />   
-               
+  return (
+    <>
+      <div className="hero-section">
+
+        {/* page header */}
+        <div className="hero-header-section">
+          <div className="hero-header">
+            <div className="header-flex">
+              <h1 className="music">
+                Music <span className="forEvery">For<br />Every</span> Milestone
+              </h1>
+              <img src="/junior/Bee.png" className="beeImg" />
+            </div>
+            
+            <img className="underlineImg" src="/junior/underline.png"/>
+            <p className="pTag">Reach your child's developmental <br />milestones with custom learning songs</p>
+          </div>
         </div>
-        </>
-        
-        
-        
-    )
+
+        <div className="every-age-every-stage">
+          <button className="button">Get Started</button>
+        </div> 
+        {/* <img src="/junior/every-age-every-stage.png" className="startSong" /> */}
+
+        {/* <div className="hero-try-section">
+
+        </div> */}
+
+          {/* <img src="/junior/polygon-bg.png" /> */}
+
+
+
+          {/* <img src="/junior/bee.gif" /> */}
+
+      </div>
+    </>
+
+
+
+  )
 
 }
 
