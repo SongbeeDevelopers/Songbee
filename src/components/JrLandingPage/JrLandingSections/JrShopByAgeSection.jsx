79bfcbd2289cac08a08d8d33f6e8b4516c097315
Leo Johnson <leochinonejohnson@gmail.com>
2024-03-17 14:12:01 -0500

    created shop by age section

---
+++ src/components/JrLandingPage/JrLandingSections/JrShopByAgeSection.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import LearnMoreButton from '../LearnMoreButton/LearnMoreButton'
+
+function JrShopByAgeSection() {
+
+  return (
+    <div className="jr-shop-by-age-bg">
+      <h2 className="jr-shop-by-age-title">Shop By Age</h2>
+
+      <div className="age-container">
+        <div className="age-item">
+          <div className="age-item-contents">
+            <h4>0-1 Years</h4>
+            <img src="/junior/shopbyageimg.png"/>
+            <p>Your baby goes through so many changes in the first year. Help them meet their milestones.</p>
+            <LearnMoreButton />
+          </div>
+        </div>
+
+        <div className="age-item">
+          <div className="age-item-contents">
+            <h4>0-1 Years</h4>
+            <img src="/junior/shopbyageimg.png"/>
+            <p>Your baby goes through so many changes in the first year. Help them meet their milestones.</p>
+            <LearnMoreButton />
+          </div>
+        </div>
+
+        <div className="age-item">
+          <div className="age-item-contents">
+            <h4>0-1 Years</h4>
+            <img src="/junior/shopbyageimg.png"/>
+            <p>Your baby goes through so many changes in the first year. Help them meet their milestones.</p>
+            <LearnMoreButton />
+          </div>
+        </div>
+
+        <div className="age-item">
+          <div className="age-item-contents">
+            <h4>0-1 Years</h4>
+            <img src="/junior/shopbyageimg.png"/>
+            <p>Your baby goes through so many changes in the first year. Help them meet their milestones.</p>
+            <LearnMoreButton />
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default JrShopByAgeSection
