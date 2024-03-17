e049870580ee1f23f2c0257b0625bd017949ab3d
Leo Johnson <leochinonejohnson@gmail.com>
2024-03-17 14:19:55 -0500

    created remaining files needed

---
+++ src/components/JrLandingPage/JrLandingSections/JrGuessworkSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JrGuessworkSection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default JrGuessworkSection
---
+++ src/components/JrLandingPage/JrLandingSections/JrReviewsSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JrReviewsSection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default JrReviewsSection
