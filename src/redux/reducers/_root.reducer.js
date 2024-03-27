7d6a83e9e2bb3f4e878954035115ae4872768ba5
Abubakar Aden <abubakar.aden978@gmail.com>
2024-03-27 16:20:09 +0300

    created sagas and generators for artist edits

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -15,6 +15,7 @@ import allArtists from "./allArtists.reducer";
 import juniorRequests from "./juniorRequest.reducer";
 import jrRequestData from "./jrRequestData.reducer";
 import artistProfile from "./artistProfile.reducer";
+import pendingEdits from "./pendingEdits.reducer";
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -38,6 +39,7 @@ const rootReducer = combineReducers({
   juniorRequests,
   jrRequestData,
   artistProfile,
+  pendingEdits,
 });
 
 export default rootReducer;
--- src/redux/sagas/artist.saga.js
+++ src/redux/sagas/artist.saga.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 import { takeLatest, put } from "redux-saga/effects";
 
-function* fetchPendingArtist () {
-    try {
-        console.log("THIS IS DATA2222");
+function* fetchPendingArtist() {
+  try {
+    console.log("THIS IS DATA2222");
 
-        const response = yield axios.get('/api/artist/pending')
-        console.log("THIS IS DATA",response.data);
-        yield put({ type: 'SET_PENDING_ARTISTS', payload: response.data})
-    }
-    catch (error) {
-        console.error('SAGA fetchPendingArtist() failed:', error)
-    }
+    const response = yield axios.get("/api/artist/pending");
+    console.log("THIS IS DATA", response.data);
+    yield put({ type: "SET_PENDING_ARTISTS", payload: response.data });
+  } catch (error) {
+    console.error("SAGA fetchPendingArtist() failed:", error);
+  }
 }
-// this generator is used to create a new artist 
+// this generator is used to create a new artist
 function* createNewArtist(action) {
   try {
     const response = yield axios({
@@ -28,76 +27,99 @@ function* createNewArtist(action) {
 }
 
 function* requestArtistEdit(action) {
+  try {
+    const response = yield axios({
+      method: "POST",
+      url: "/api/artist/edit",
+      data: action.payload,
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error("SAGA createNewArtist() failed:", error);
+  }
+}
+
+function* approveArtist(action) {
+  try {
+    yield axios.put(`/api/artist/${action.payload.id}`, {
+      user_id: action.payload.user_id,
+    });
+    yield put({ type: "FETCH_PENDING_ARTISTS" });
+  } catch (error) {
+    console.error("SAGA approveArtist() failed:", error);
+  }
+}
+
+function* getArtistDetails() {
+  try {
+    const response = yield axios.get(`/api/artist/get`);
+    yield put({ type: "SET_ARTIST_PROFILE", payload: response.data[0] });
+  } catch (error) {
+    console.error("SAGA approveArtist() failed:", error);
+  }
+}
+
+function* getPendingEdits() {
+  try {
+    const response = yield axios.get(`/api/artist/pending-edits`);
+    yield put({ type: "SET_PENDING_EDITS", payload: response.data });
+  } catch (error) {
+    console.error("SAGA approveArtist() failed:", error);
+  }
+}
+
+function* approveEditArtistInfo(action) {
+  try {
+    yield axios.put(`/api/artist/approve/${action.payload.id}`);
+    yield put({ type: "GET_ARTIST_PENDING" });
+  } catch (error) {
+    console.error("SAGA approveArtist() failed:", error);
+  }
+}
+
+function* denyEditArtistInfo(action) {
     try {
-      const response = yield axios({
-        method: "POST",
-        url: "/api/artist/edit",
-        data: action.payload,
-      });
-      console.log(response.data);
+      yield axios.delete(`/api/artist/deny/${action.payload}`);
+      yield put({ type: "GET_ARTIST_PENDING" });
     } catch (error) {
-      console.error("SAGA createNewArtist() failed:", error);
+      console.error("SAGA approveArtist() failed:", error);
     }
   }
 
-function* approveArtist(action){
-    try {
-        yield axios.put(`/api/artist/${action.payload.id}`, {user_id: action.payload.user_id})
-        yield put({ type: 'FETCH_PENDING_ARTISTS'})
-    } catch (error) {
-        console.error('SAGA approveArtist() failed:', error)
-    }
+function* deleteArtist(action) {
+  try {
+    yield axios.delete(`/api/artist/${action.payload}`);
+    yield put({ type: "FETCH_PENDING_ARTISTS" });
+  } catch (error) {
+    console.error("SAGA deleteArtist() failed:", error);
+  }
 }
 
-function* getArtistDetails(){
-    try {
-     const response =  yield axios.get(`/api/artist/get`)
-        yield put({ type: 'SET_ARTIST_PROFILE', payload: response.data[0]})
-    } catch (error) {
-        console.error('SAGA approveArtist() failed:', error)
-    }
-}
-
-function* approveEditArtistInfo(action){
-    try {
-        yield axios.put(`/api/artist/approve/${action.payload.id}`)
-        yield put({ type: 'FETCH_PENDING_ARTISTS'})
-    } catch (error) {
-        console.error('SAGA approveArtist() failed:', error)
-    }
-}
-function* deleteArtist(action){
-    try {
-        yield axios.delete(`/api/artist/${action.payload}`)
-        yield put({ type: 'FETCH_PENDING_ARTISTS'})
-    } catch (error) {
-        console.error('SAGA deleteArtist() failed:', error)
-    }
-}
-
-function* fetchAllArtists(){
-    try {
-        const response = yield axios.get('/api/artist/all')
-        yield put({
-            type: "SET_ALL_ARTISTS",
-            payload: response.data
-        })
-    } catch (error) {
-        console.error('SAGA fetchAllArtists() failed:', error)
-    }
+function* fetchAllArtists() {
+  try {
+    const response = yield axios.get("/api/artist/all");
+    yield put({
+      type: "SET_ALL_ARTISTS",
+      payload: response.data,
+    });
+  } catch (error) {
+    console.error("SAGA fetchAllArtists() failed:", error);
+  }
 }
 
 function* artistSaga() {
   yield takeLatest("CREATE_ARTIST", createNewArtist);
-  yield takeLatest('FETCH_PENDING_ARTISTS', fetchPendingArtist);
-  yield takeLatest('APPROVE_ARTIST', approveArtist);
-  yield takeLatest('DELETE_ARTIST', deleteArtist);
-  yield takeLatest('FETCH_ALL_ARTISTS', fetchAllArtists);
-  yield takeLatest('APPROVE_EDIT_ARTIST', approveEditArtistInfo);
-  yield takeLatest('REQUEST_ARTIST_EDIT', requestArtistEdit);
-  yield takeLatest('GET_ARTIST_PROFILE', getArtistDetails);
+  yield takeLatest("FETCH_PENDING_ARTISTS", fetchPendingArtist);
+  yield takeLatest("APPROVE_ARTIST", approveArtist);
+  yield takeLatest("DELETE_ARTIST", deleteArtist);
+  yield takeLatest("FETCH_ALL_ARTISTS", fetchAllArtists);
+  yield takeLatest("APPROVE_EDIT_ARTIST", approveEditArtistInfo);
+  yield takeLatest("REQUEST_ARTIST_EDIT", requestArtistEdit);
+  yield takeLatest("GET_ARTIST_PROFILE", getArtistDetails);
+  yield takeLatest("GET_ARTIST_PENDING", getPendingEdits);
+  yield takeLatest("DENY_EDIT_ARTIST", denyEditArtistInfo);
 
-
+  
 }
 
 export default artistSaga;
