46ddf9d175c12c3d5fdba41b4b7c7bc539ecc199
Leo Johnson <leochinonejohnson@gmail.com>
2024-03-28 10:36:27 -0500

    format

--- src/components/JrLandingPage/JrLandingSections/JrBenefitsSection.jsx
+++ src/components/JrLandingPage/JrLandingSections/JrBenefitsSection.jsx
@@ -9,6 +9,7 @@ function BenefitsSection() {
        <div className="jr-benefits-bg">
         <div className="jr-benefits">
           <h2>The Benefits</h2>
+          
           <img src="/junior/benefits.png"/>
         </div>
       </div>
