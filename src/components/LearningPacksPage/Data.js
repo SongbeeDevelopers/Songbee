57749b9e54845bf769a6edf32eb0b68ea5a92ccc
Abubakar Aden <abubakar.aden978@gmail.com>
2024-04-05 13:07:36 +0300

    created learning packs data and info

---
+++ src/components/LearningPacksPage/Data.js
@@ -0,0 +1,236 @@
+export const Learningpacks = [
+  {
+    id: 0,
+    name: "0-12 months",
+    description: "Engage both sides of the brain and discover.",
+    delivery: "Delivers | every 2 months",
+    pricing: "$80/Pack",
+    gridItems: 3,
+    scrollId: "0_12_months",
+    items: [
+      {
+        id: 0,
+        imageUrl: "/junior/0-12/babyOne.jpg",
+        name: "The Bonder",
+        age: "0-12 Weeks",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/0-12/babyTwo.jpg",
+        name: "The Feeler",
+        age: "3-4 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/0-12/babyThree.jpg",
+        name: "The Detector",
+        age: "5-6 months",
+      },
+
+      {
+        id: 0,
+        imageUrl: "/junior/0-12/babyFour.jpg",
+        name: "The Experimenter",
+        age: "7-8 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/0-12/babyFive.jpg",
+        name: "The Investigator",
+        age: "9-10 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/0-12/babySix.jpg",
+        name: "The Discoverer",
+        age: "11-12 months",
+      },
+    ],
+  },
+
+  {
+    id: 1,
+    name: "1 year",
+    description: "Build new skills and play to learn.",
+    delivery: "Delivers | every 2 months",
+    pricing: "$80/Pack",
+    gridItems: 3,
+    scrollId: "1_year",
+    items: [
+      {
+        id: 0,
+        imageUrl: "/junior/1year/babyOne.jpg",
+        name: "The Talker",
+        age: "13-14 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/1year/babyTwo.jpg",
+        name: "The Explorer",
+        age: "15-16 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/1year/babyThree.jpg",
+        name: "The Pathfinder",
+        age: "17-18 months",
+      },
+
+      {
+        id: 0,
+        imageUrl: "/junior/1year/babyFour.jpg",
+        name: "The Looker",
+        age: "19-20 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/1year/babyFive.jpg",
+        name: "The Socializer",
+        age: "21-22 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/1year/babySix.jpg",
+        name: "The Puzzler",
+        age: "23-24 months",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "2 years",
+    description: "Stimulate the brain and start making connections.",
+    delivery: "Delivers | every 2 months",
+    pricing: "$120/Pack",
+    gridItems: 2,
+    scrollId: "2_year",
+    items: [
+      {
+        id: 0,
+        imageUrl: "/junior/2year/babyOne.jpg",
+        name: "The Experimenter",
+        age: "25-26 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/2year/babyTwo.jpg",
+        name: "The Story Teller",
+        age: "27-28 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/2year/babyThree.jpg",
+        name: "The Creator",
+        age: "29-30 months",
+      },
+
+      {
+        id: 0,
+        imageUrl: "/junior/2year/babyFour.jpg",
+        name: "The Adventurer",
+        age: "31-32 months",
+      },
+      //   { id: 1, imageUrl: "/junior/2year/babyFive.jpg", name: "The Problem Solver", age: "33-34 months" },
+      //   { id: 2, imageUrl: "/junior/2year/babySix.jpg", name: "The Blossomer", age: "35-36 months" },
+    ],
+  },
+  {
+    id: 3,
+    name: "3 year",
+    description: "Stimulate the brain and start making connections.",
+    delivery: "Delivers | every 2 months",
+    pricing: "$120/Pack",
+    gridItems: 2,
+    scrollId: "3_year",
+
+    items: [
+      {
+        id: 0,
+        imageUrl: "/junior/1year/babyOne.jpg",
+        name: "The Talker",
+        age: "13-14 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/1year/babyTwo.jpg",
+        name: "The Explorer",
+        age: "15-16 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/1year/babyThree.jpg",
+        name: "The Pathfinder",
+        age: "17-18 months",
+      },
+
+      {
+        id: 0,
+        imageUrl: "/junior/1year/babyFour.jpg",
+        name: "The Looker",
+        age: "19-20 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/1year/babyFive.jpg",
+        name: "The Socializer",
+        age: "21-22 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/1year/babySix.jpg",
+        name: "The Puzzler",
+        age: "23-24 months",
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    name: "4 years",
+    description: "Stimulate the brain and start making connections.",
+    delivery: "Delivers | every 2 months",
+    pricing: "$120/Pack",
+    gridItems: 2,
+    scrollId: "4_year",
+    items: [
+      {
+        id: 0,
+        imageUrl: "/junior/1year/babyOne.jpg",
+        name: "The Talker",
+        age: "13-14 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/1year/babyTwo.jpg",
+        name: "The Explorer",
+        age: "15-16 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/1year/babyThree.jpg",
+        name: "The Pathfinder",
+        age: "17-18 months",
+      },
+
+      {
+        id: 0,
+        imageUrl: "/junior/1year/babyFour.jpg",
+        name: "The Looker",
+        age: "19-20 months",
+      },
+      {
+        id: 1,
+        imageUrl: "/junior/1year/babyFive.jpg",
+        name: "The Socializer",
+        age: "21-22 months",
+      },
+      {
+        id: 2,
+        imageUrl: "/junior/1year/babySix.jpg",
+        name: "The Puzzler",
+        age: "23-24 months",
+      },
+    ],
+  },
+];
