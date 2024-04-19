c859c80b1c79d6b7a03e588cb8fd97993a419e35
Leo Johnson <leochinonejohnson@gmail.com>
2024-04-19 07:59:57 -0500

    format

--- database.sql
+++ database.sql
@@ -1,54 +1,54 @@
 DROP TABLE IF EXISTS "songbeejr_details",
-                    "subscription_song_details",
-                    "subscription",
-                    "jr_request",
-                    "artist_genres",
-                    "song_details",
-                    "artist",
-                    "song_request",
-                    "genres",
-                    "user",
-                    "documents";
-
+                     "subscription_song_details",
+                     "subscription",
+                     "jr_request",
+                     "artist_genres",
+                     "song_details",
+                     "artist",
+                     "song_request",
+                     "genres",
+                     "user",
+                     "documents",
+                     "pendingartistedit";
 
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "email" VARCHAR (150) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL,
-    "class" INT NOT NULL DEFAULT 1,
-    "name" VARCHAR (150),
-    "credit" INT,
-    "created_at" TIMESTAMPTZ DEFAULT NOW()
+  "id" SERIAL PRIMARY KEY,
+  "email" VARCHAR (150) UNIQUE NOT NULL,
+  "password" VARCHAR (1000) NOT NULL,
+  "class" INT NOT NULL DEFAULT 1,
+  "name" VARCHAR (150),
+  "credit" INT,
+  "created_at" TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE "genres" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (80) UNIQUE NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR (80) UNIQUE NOT NULL
 );
 
 CREATE TABLE "song_request" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" integer REFERENCES "user",
-    "genre_id" integer REFERENCES "genres",
-    "requester" VARCHAR (150),
-    "recipient" VARCHAR (150),
-    "pronunciation" VARCHAR (150),
-    "recipient_relationship" VARCHAR (150),
-    "occasion" VARCHAR (150) DEFAULT NULL,
-    "vocal_type" VARCHAR (150),
-    "vibe" VARCHAR (150),
-    "tempo" VARCHAR (150),
-    "inspiration" VARCHAR,
-    "story1" VARCHAR,
-    "story2" VARCHAR,
-    "important_what" VARCHAR,
-    "important_why" VARCHAR,
-    "additional_info" VARCHAR,
-    "created_at" TIMESTAMPTZ DEFAULT NOW(),
-    "delivery_days" integer,
-    "streaming" BOOLEAN,
-    "extra_verse" BOOLEAN,
-    "is_complete" BOOLEAN DEFAULT FALSE
+  "id" SERIAL PRIMARY KEY,
+  "user_id" integer REFERENCES "user",
+  "genre_id" integer REFERENCES "genres",
+  "requester" VARCHAR (150),
+  "recipient" VARCHAR (150),
+  "pronunciation" VARCHAR (150),
+  "recipient_relationship" VARCHAR (150),
+  "occasion" VARCHAR (150) DEFAULT NULL,
+  "vocal_type" VARCHAR (150),
+  "vibe" VARCHAR (150),
+  "tempo" VARCHAR (150),
+  "inspiration" VARCHAR,
+  "story1" VARCHAR,
+  "story2" VARCHAR,
+  "important_what" VARCHAR,
+  "important_why" VARCHAR,
+  "additional_info" VARCHAR,
+  "created_at" TIMESTAMPTZ DEFAULT NOW(),
+  "delivery_days" integer,
+  "streaming" BOOLEAN,
+  "extra_verse" BOOLEAN,
+  "is_complete" BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE "artist" (
@@ -74,13 +74,13 @@ CREATE TABLE "artist" (
 );
 
 CREATE TABLE "song_details" (
-    "id" SERIAL PRIMARY KEY,
-    "song_request_id" integer REFERENCES "public"."song_request"("id") ON DELETE CASCADE,
-    "url" VARCHAR (500),
-    "lyrics" VARCHAR,
-    "title" VARCHAR (250),
-    "artist_id" integer REFERENCES "artist" ON DELETE CASCADE,
-    "streaming_link" VARCHAR
+  "id" SERIAL PRIMARY KEY,
+  "song_request_id" integer REFERENCES "public"."song_request"("id") ON DELETE CASCADE,
+  "url" VARCHAR (500),
+  "lyrics" VARCHAR,
+  "title" VARCHAR (250),
+  "artist_id" integer REFERENCES "artist" ON DELETE CASCADE,
+  "streaming_link" VARCHAR
 );
 
 CREATE TABLE "artist_genres" (
@@ -105,8 +105,6 @@ CREATE TABLE "jr_request" (
   "created_at" TIMESTAMPTZ DEFAULT NOW(),
    "is_complete" BOOLEAN DEFAULT FALSE,
      "accepted"  BOOLEAN DEFAULT FALSE
-
-
 );
 
 CREATE TABLE "subscription" (
@@ -146,8 +144,7 @@ VALUES
 ('berchmanpaul@gmail.com', '$2a$10$9bWLNdvC42J3xoCKYD.JtuFt0eD9Zxn7FqkuV.Fy6F.efBhs6CoYC', 2),
 ('perrin.xthona@gmail.com', '$2a$10$YsNEzVgTG29iizCMzHaPpuUGQDgQ7.CGae6OJq0oKOFvXtonvy6V2', 2),
 ('lefevre.michaelj@gmail.com', '$2a$10$S/gUrKEDMRPAQigNo5cBcuyDAQDu1PXkbn6zjQNTqHclshKlpZLqe', 2);
-
---Password for songbee account is: password
+--Password for songbee account is: Prime#0101
 
 INSERT INTO "genres"
 VALUES
@@ -157,23 +154,23 @@ INSERT INTO "artist"
 ("artist_name", "name", "user_id", "vocal_type", "approved", "is_active", "website", "instagram_link", "sample_song_1", "sample_song_2", "sample_song_3", "bio","location", "photo", "streaming_link")
 VALUES
 ('Michael LeFerve', 'Michael Leferve', 7, 'male', TRUE, TRUE, '', 'https://www.instagram.com/thefevaa/', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321165/Michael_Leferve_Summers_With_You_jxknhm.mp3', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321147/Blossoming_by_Michael_leferve_v6ylay.mp3', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321186/Michael-LeFevre-smile_kt9evo.mp3', 'Michael LeFevre is the lead singer and songwriter of None The Younger and Sleepy Soul. 
-          While he has been writing music for well over a decade, he has been producing and recording out of his studio full time for the last 4 years.
-          This has given him the experiences necessary to really perfect his craft. 
-          Since his time as a full-time musician, he has been featured in Spotify and Youtube Editorial playlists, had many magazine write-ups, and accumulated millions of streams across all platforms.
-          He looks forward to continuing to write and spread joy through music.', 'St. Louis, Missouri', 'https://res.cloudinary.com/dke4ukd0z/image/upload/v1713458694/michael-photo_edh5b9.jpg', 'https://open.spotify.com/artist/32SXond7qEk5OOXU9M8Sq7?si=MZ2frkvaR6OUOmIYwGKd5w&nd=1&dlsi=3dca887b59374406/'),
+  While he has been writing music for well over a decade, he has been producing and recording out of his studio full time for the last 4 years.
+  This has given him the experiences necessary to really perfect his craft. 
+  Since his time as a full-time musician, he has been featured in Spotify and Youtube Editorial playlists, had many magazine write-ups, and accumulated millions of streams across all platforms.
+  He looks forward to continuing to write and spread joy through music.', 'St. Louis, Missouri', 'https://res.cloudinary.com/dke4ukd0z/image/upload/v1713458694/michael-photo_edh5b9.jpg', 'https://open.spotify.com/artist/32SXond7qEk5OOXU9M8Sq7?si=MZ2frkvaR6OUOmIYwGKd5w&nd=1&dlsi=3dca887b59374406/'),
 
 ('Berch', 'Berch', 1, 'male', TRUE, TRUE, '', 'https://www.instagram.com/mynameisberch/?igshid=MzMyNGUyNmU2YQ%3D%3D&utm_source=qr', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321136/Berch_m2vbfj.mp3', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321126/Berch_fee_fi_fo_fum_lgnkd3.mp3', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321572/Berch_feel_so_good_kwia9r.mp3', 'Mynameisberch, hailing from Richmond, Virginia, is a versatile artist known for his pop,
-             hip-hop, and soulful vocal style. With influences ranging from John Mayer to Lenny Kravitz and Drake, he effortlessly blends catchy melodies and heartfelt lyrics into his captivating compositions. 
-             As a published BMI songwriter, mynameisberch consistently delivers multiple songs and records each year, showcasing his dedication to his craft. Additionally, 
-             he has been recognized as a featured songwriter in Worship Leader magazine,
-             a national publication in the contemporary Christian music scene. Combining his musical talents with his role as a worship leader at his church, mynameisberch/s passion for creating unique and impactful music shines through.
-             With a genuine 
-             desire to connect with individuals going through various situations and seasons, mynameisberch is excited to bring his creativity to Songbee, crafting personalized songs that resonate with each listener. He says, 
-             "I love creating for unique people going through unique situations in unique seasons.', 'Richmond, Virginia', 'https://res.cloudinary.com/dke4ukd0z/image/upload/v1713458683/berch_bdrlty.jpg', 'https://mynameisberch.bandcamp.com/'),
+  hip-hop, and soulful vocal style. With influences ranging from John Mayer to Lenny Kravitz and Drake, he effortlessly blends catchy melodies and heartfelt lyrics into his captivating compositions. 
+  As a published BMI songwriter, mynameisberch consistently delivers multiple songs and records each year, showcasing his dedication to his craft. Additionally, 
+  he has been recognized as a featured songwriter in Worship Leader magazine,
+  a national publication in the contemporary Christian music scene. Combining his musical talents with his role as a worship leader at his church, mynameisberch/s passion for creating unique and impactful music shines through.
+  With a genuine 
+  desire to connect with individuals going through various situations and seasons, mynameisberch is excited to bring his creativity to Songbee, crafting personalized songs that resonate with each listener. He says, 
+  "I love creating for unique people going through unique situations in unique seasons.', 'Richmond, Virginia', 'https://res.cloudinary.com/dke4ukd0z/image/upload/v1713458683/berch_bdrlty.jpg', 'https://mynameisberch.bandcamp.com/'),
 
 ('Perrin Xthona', 'Perrin Xthona', 7, 'female', TRUE, TRUE, 'https://www.tiktok.com/@perrinxthona?_t=8hjAn9c36du&_r=1/', 'https://www.instagram.com/perrinxthona/', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321199/Perrin_Xthona_faqiwx.mp3', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321212/to_the_moon_and_back_PerrinXthona_wfqrgi.mp3', 'https://res.cloudinary.com/dke4ukd0z/video/upload/v1713321226/You_Are_My_Home_PerrinXthona_vdkbkt.mp3', 'Perrin Xthona is a pop songwriter from Portland, Oregon. She recently graduated from the Berklee College of Music, and her influences are Julia Michaels, 
-                        Lennon Stella, Halsey, Jeremy Zucker, Lauv, and Harry Styles. 
-                        She/s always loved writing songs, and the only thing she loves more is getting to hear other peoples stories.', 'Portland, Oregon', 'https://res.cloudinary.com/dke4ukd0z/image/upload/v1713458705/perrin_photo_mhtkti.jpg', 'https://open.spotify.com/artist/1bNx6UhsCYSNuoIeL9LcnD?si=MLyNUmi6RJm2qLdyip1vMA&nd=1&dlsi=27bc328009f646c5/');
+  Lennon Stella, Halsey, Jeremy Zucker, Lauv, and Harry Styles. 
+  She/s always loved writing songs, and the only thing she loves more is getting to hear other peoples stories.', 'Portland, Oregon', 'https://res.cloudinary.com/dke4ukd0z/image/upload/v1713458705/perrin_photo_mhtkti.jpg', 'https://open.spotify.com/artist/1bNx6UhsCYSNuoIeL9LcnD?si=MLyNUmi6RJm2qLdyip1vMA&nd=1&dlsi=27bc328009f646c5/');
 
 INSERT INTO "song_request"
 ("user_id", "genre_id", "requester", "recipient", "pronunciation", "recipient_relationship", "occasion", "vocal_type", "vibe", "tempo", "inspiration", "story1", "story2", "important_what", "important_why", "additional_info", "created_at", "delivery_days", "streaming", "extra_verse", "is_complete")
@@ -227,10 +224,6 @@ INSERT INTO "jr_request" (
   'Improve singing skills' -- Replace with the actual goals
 );
 
-
-
-
-
 INSERT INTO "jr_request" (
   "user_id",
   "requester",
@@ -252,32 +245,27 @@ INSERT INTO "jr_request" (
 (1, 'Bob Johnson', 'Emily', 'Em-i-ly', 12, 'Dancing', 'Energetic', 'Fast', 'Mezzo-Soprano', 'Choreography for Emily', 'Win a dance competition', FALSE, TRUE),
 (1, 'Alice Brown', 'Daniel', 'Dan-yel', 9, 'Drawing', 'Focused', 'Steady', 'Tenor', 'Drawing lessons for Daniel', 'Develop artistic skills', FALSE, FALSE);
 
-
-
 CREATE TABLE "songbeejr_details" (
-    "id" SERIAL PRIMARY KEY,
-    "jr_request_id" integer REFERENCES "public"."jr_request"("id") ON DELETE CASCADE,
-    "url" VARCHAR (500),
-    "lyrics" VARCHAR,
-    "title" VARCHAR (250),
-    "artist_id" integer REFERENCES "artist",
-    "streaming_link" VARCHAR
+  "id" SERIAL PRIMARY KEY,
+  "jr_request_id" integer REFERENCES "public"."jr_request"("id") ON DELETE CASCADE,
+  "url" VARCHAR (500),
+  "lyrics" VARCHAR,
+  "title" VARCHAR (250),
+  "artist_id" integer REFERENCES "artist",
+  "streaming_link" VARCHAR
 );
-
 
 INSERT INTO "songbeejr_details" ("jr_request_id") VALUES (1);
    
-
-
 CREATE TABLE "pendingartistedit" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_id" INTEGER REFERENCES "artist"(id) ON DELETE CASCADE,
-    "edited_name" VARCHAR(255),
-    "edited_artistName" VARCHAR(255),
-    "edited_bio" TEXT,
-    "edited_website" VARCHAR(255),
-    "edited_vocal_type" VARCHAR(100),
-    "edited_genre_id" INTEGER REFERENCES "genres"(id),
-    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  "id" SERIAL PRIMARY KEY,
+  "artist_id" INTEGER REFERENCES "artist"(id) ON DELETE CASCADE,
+  "edited_name" VARCHAR(255),
+  "edited_artistName" VARCHAR(255),
+  "edited_bio" TEXT,
+  "edited_website" VARCHAR(255),
+  "edited_vocal_type" VARCHAR(100),
+  "edited_genre_id" INTEGER REFERENCES "genres"(id),
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
    
