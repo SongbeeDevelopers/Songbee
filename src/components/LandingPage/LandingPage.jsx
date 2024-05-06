243524b8beb18a7492e0565275c8829936e7e11a
Leo Johnson <leochinonejohnson@gmail.com>
2024-05-06 14:21:07 -0500

    changed filenames

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
-import HeroSection from "./LandingPageSections/HeroSection";
-import HowSongBeeWorksSection from "./LandingPageSections/HowSongbeeWorksSection";
-import TestimonialsSection from "./LandingPageSections/TestimonialsSection";
-import SampleSongsSection from "./LandingPageSections/SampleSongsSection";
-import GuaranteeSection from "./LandingPageSections/GuaranteeSection";
+import HeroSection from "./LandingPageSections/1-HeroSection";
+import HowSongBeeWorksSection from "./LandingPageSections/2-HowSongbeeWorksSection";
+import TestimonialsSection from "./LandingPageSections/3-TestimonialsSection";
+import SampleSongsSection from "./LandingPageSections/4-SampleSongsSection";
+import GuaranteeSection from "./LandingPageSections/6-GuaranteeSection";
 
 import "./LandingPage.css";
 
@@ -28,6 +28,7 @@ function LandingPage({ routeVariants }) {
       {/* <SampleSongsSection /> */}
 
       <div className="guarantee-container">
+
         <GuaranteeSection />
       </div>
 
