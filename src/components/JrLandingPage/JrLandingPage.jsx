72ad3745f3d84188c749b7e63f10e074513142dd
Leo Johnson <leochinonejohnson@gmail.com>
2024-05-06 15:36:51 -0500

    filetree stuff

--- src/components/JrLandingPage/JrLandingPage.jsx
+++ src/components/JrLandingPage/JrLandingPage.jsx
@@ -4,16 +4,16 @@ import { motion } from "framer-motion";
 
 import "./JrLandingPage.css";
 
-import JrHeroSection from "./JrLandingSections/JrHeroSection";
-import JrJoinSection from "./JrLandingSections/JrJoinSection";
-import JrWorksSection from "./JrLandingSections/JrWorksSection";
-import JrBenefitsSection from "./JrLandingSections/JrBenefitsSection";
-import WhyJuniorSection from "./JrLandingSections/JrWhySection"
-import JrLearningPacksSection from "./JrLandingSections/JrLearningPacksSection";
-import JrOfferSection from "./JrLandingSections/JrMoreThanMusic";
-import JrGuessworkSection from "./JrLandingSections/JrGuessworkSection";
-import JrReviewsSection from "./JrLandingSections/JrReviewsSection";
-import JrNewsletterSection from "./JrLandingSections/JrNewsletterSection";
+import JrHeroSection from "./JrLandingSections/1-JrHeroSection";
+import JrJoinSection from "./JrLandingSections/2-JrJoinSection";
+import JrWorksSection from "./JrLandingSections/4-JrWorksSection";
+import JrBenefitsSection from "./JrLandingSections/3-JrBenefitsSection";
+import WhyJuniorSection from "./JrLandingSections/5-JrWhySection"
+import JrLearningPacksSection from "./JrLandingSections/6-JrLearningPacksSection";
+import JrOfferSection from "./JrLandingSections/7-JrMoreThanMusic";
+import JrGuessworkSection from "./JrLandingSections/8-JrGuessworkSection";
+import JrReviewsSection from "./JrLandingSections/10-JrReviewsSection";
+import JrNewsletterSection from "./JrLandingSections/9-JrNewsletterSection";
 import FaqPageSbJR from '../InfoPages/FaqPageSbJR'
 
 
