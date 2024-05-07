03a87e8b84823c8bec5fff673d11c009ca4dc0bc
Walker Neudorff <walkerneudorff@gmail.com>
2024-05-07 12:13:09 -0500

    Created calendar buttons for delivery days, added checkboxes and conditional rendering for checks

--- src/components/OrderPage/OrderFormSteps/4-Delivery.jsx
+++ src/components/OrderPage/OrderFormSteps/4-Delivery.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { Button } from "@mui/material";
+import { Button, Checkbox } from "@mui/material";
 
-import '../../SongRequestPage/SongRequestPage.css'
-
+import "../../SongRequestPage/SongRequestPage.css";
 
 export default function Delivery({ handleInput, handleOpen }) {
-
   const user = useSelector((store) => store.user);
+  const requestData = useSelector((store) => store.requestData);
 
   // date logic
   const now = new Date();
@@ -19,27 +18,59 @@ export default function Delivery({ handleInput, handleOpen }) {
 
   return (
     <form className="orderForm">
-      <div className="reqFormGroup">
-        <div className="reqFormInput">
-          <label>When would you like your song delivered?</label>
-          
-          <Button variant="contained"
+      <div className="reqFormGroup4">
+        <label>When would you like your song delivered?</label>
+        <div className="reqFormDays">
+          <div 
+            className="calendar-img-div"
             onClick={() => handleInput("delivery_days", 3)}
-            sx={{ height: 35, backgroundColor: "#feaf17", color: "black" }}
-          > {new Date(threeDays).toDateString()} + $80
-          </Button>
+          >
+            <Checkbox
+              checked={requestData.delivery_days === 3}
+              sx={{ mb: -10, ml: -5, mr: 8, backgroundColor: "#fff4df" }}
+            />
+            <img
+              className="calendar-img"
+              src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1715096840/vecteezy_calendar-icon-clipart-design-illustration_9380244_qcc9sv.png"
+            ></img>
 
-          <Button variant="contained"
-            onClick={() => handleInput("delivery_days", 5)}
-            sx={{ height: 35, backgroundColor: "#feaf17", color: "black" }}
-          > {new Date(fiveDays).toDateString()} + $40
-          </Button>
+            <div className="calendar-txt">
+              {new Date(threeDays).toDateString()} <p>3 Days + $80</p>
+            </div>
+          </div>
 
-          <Button variant="contained"
-            onClick={() => handleInput("delivery_days", 6)}
-            sx={{ height: 35, backgroundColor: "#feaf17", color: "black" }}
-          > {new Date(sixDays).toDateString()} + $0
-          </Button>
+          <div 
+            className="calendar-img-div"
+            onClick={() => handleInput("delivery_days", 5)}>
+          <Checkbox
+              checked={requestData.delivery_days === 5}
+              sx={{ mb: -10, ml: -5, mr: 8, backgroundColor: "#fff4df" }}
+            />
+            <img
+              className="calendar-img"
+              src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1715096840/vecteezy_calendar-icon-clipart-design-illustration_9380244_qcc9sv.png"
+            ></img>
+
+            <div className="calendar-txt">
+              {new Date(fiveDays).toDateString()} <p>5 Days + $40</p>
+            </div>
+          </div>
+
+          <div 
+            className="calendar-img-div"
+            onClick={() => handleInput("delivery_days", 6)}>
+          <Checkbox
+              checked={requestData.delivery_days === 6}
+              sx={{ mb: -10, ml: -5, mr: 8, backgroundColor: "#fff4df" }}
+            />
+            <img
+              className="calendar-img"
+              src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1715096840/vecteezy_calendar-icon-clipart-design-illustration_9380244_qcc9sv.png"
+            ></img>
+            <div className="calendar-txt">
+              {new Date(sixDays).toDateString()} <p>6 Days + $0</p>
+            </div>
+          </div>
         </div>
       </div>
       {!user.id && (
@@ -48,5 +79,5 @@ export default function Delivery({ handleInput, handleOpen }) {
         </button>
       )}
     </form>
-  )
+  );
 }
