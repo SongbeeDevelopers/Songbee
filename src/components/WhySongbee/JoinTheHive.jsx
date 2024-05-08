71ba9c33d20d5bcf8dc7c697ed25be986c0945da
Abubakar Aden <abubakar.aden978@gmail.com>
2024-05-08 10:13:04 +0300

    created new component

---
+++ src/components/WhySongbee/JoinTheHive.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./WhySongbee.css";
+import { Link } from "react-router-dom";
+
+function JoinTheHive() {
+  return (
+    <div className="join-the-hive">
+      <div className="left-hive">
+        <img
+          src="/junior/hive-imgs/e1b45e78-7b43-4ce8-82d0-41f1438d3991.png"
+          alt=""
+        />
+        <h2>
+       
+          <span className="highlight">Join</span> the hive,<br /> learn the benefits.
+        </h2>
+      </div>
+      <div className="right-hive">
+        <div className="bs-rightCard jth-rightcard">
+          <div className="bs-rightImage">
+            <img
+              src="/junior/hive-imgs/hive-pic-1.webp"
+              alt=""
+            />
+          </div>
+          <p>Emotional</p>
+        </div>
+
+
+        <div className="bs-rightCard jth-rightcard">
+          <div className="bs-rightImage">
+            <img
+              src="/junior/hive-imgs/hive-img-2.webp"
+              alt=""
+            />
+          </div>
+          <p>Cognitive</p>
+        </div>
+
+
+
+        <div className="bs-rightCard jth-rightcard">
+          <div className="bs-rightImage">
+            <img
+              src="/junior/hive-imgs/hive-img-3.webp"
+              alt=""
+            />
+          </div>
+          <p>Social</p>
+        </div>
+
+
+
+        <div className="bs-rightCard jth-rightcard">
+          <div className="bs-rightImage">
+            <img
+              src="/junior/hive-imgs/hive-img-4.webp"
+              alt=""
+            />
+          </div>
+          <p>Behavioral</p>
+
+
+        </div>
+       <Link to="/si-benefits" className="jr-landing-btn hive-button">Learn More</Link> 
+      </div>
+    </div>
+  );
+}
+
+export default JoinTheHive;
