63039e42598fe27e5765657387e77e4965e1e319
Leo Johnson <leochinonejohnson@gmail.com>
2024-05-14 14:53:27 -0500

    replaced review text

--- src/components/JrLandingPage/JrLandingSections/10-JrReviewsSection.jsx
+++ src/components/JrLandingPage/JrLandingSections/10-JrReviewsSection.jsx
@@ -6,33 +6,48 @@ function JrReviewsSection() {
 
   const history = useHistory()
 
+  const reviews = [
+    {
+      name: 'Emily F.',
+      role: 'Education Specialist',
+      text: `
+      Songbee provides such an amazing opportunity to pair music with learning, social/emotional development, and fun!
+      `,
+    },
+    {
+      name: 'Amrit D., PHD',
+      role: 'Student Health & Wellness',
+      text: `
+      As an educator, I love working with Songbee. The learning and clinical applications 
+      of the songs written for our Lower School are extremely broad and are enhancing many 
+      of our existing programs and services.
+      `,
+    },
+    {
+      name: 'Erin L.',
+      role: 'Educator',
+      text: `
+      Songbee's music is inspiring, fun to listen to, has a sincere and genuine message that 
+      all children, teachers, caretakers, parents, and guardians can relate to for many situations. 
+      I would highly recommend their music to all age groups!
+      `,
+    },
+  ]
+
   return (
     <div className="jr-reviews-bg">
       <div className="jr-reviews">
         <h2 className="jr-reviews-header">Why families love us!</h2>
 
         <div className="jr-review-item-container">
-          <div className="jr-review-item">
-            <h3>Example Name</h3>
-            <img src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714070494/Songbee/star-one_ohfquo.png" />
-            <p>
-              "Songbee JR is the best! SongBee is the best! SongBee is the best!"
-            </p>
-          </div>
-          <div className="jr-review-item">
-            <h3>Example Name</h3>
-            <img src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714070494/Songbee/star-one_ohfquo.png" />
-            <p>
-              "Songbee JR is the best! SongBee is the best! SongBee is the best!"
-            </p>
-          </div>
-          <div className="jr-review-item">
-            <h3>Example Name</h3>
-            <img src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714070494/Songbee/star-one_ohfquo.png" />
-            <p>
-              "Songbee JR is the best! SongBee is the best! SongBee is the best!"
-            </p>
-          </div>
+          {reviews.map((review) => (
+              <div className="jr-review-item">
+                <h3>{review.name}</h3>
+                <p>{review.role}</p>
+                <img src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714070494/Songbee/star-one_ohfquo.png" />
+                <p>{review.text}</p>
+              </div>
+            ))}
         </div>
 
         <button id="guesswork-btn" className="jr-landing-btn" onClick={() => history.push('/why-songbee')}>
