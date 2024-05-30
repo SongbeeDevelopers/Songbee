52ad23224cdf2aff5813e350782eab7e1f5d14f3
Walker Neudorff <walkerneudorff@gmail.com>
2024-05-30 12:35:48 -0500

    Updated server logic and client side code for learning pack song uploads

--- src/components/UserPortals/AdminPortal/AdminPortalTabs/AdminPortalDialogs/LearningPackDialog.jsx
+++ src/components/UserPortals/AdminPortal/AdminPortalTabs/AdminPortalDialogs/LearningPackDialog.jsx
@@ -36,9 +36,141 @@ export default function LearningPackDialog({ setDetailsOpen }) {
     dispatch({type: 'EDIT_INPUT', payload: {key, value}})
   }
 
+  const uploadButton = (value) => {
+    if(value === 'song1'){
+        detailsForm.append('file', songFile1);
+        detailsForm.append("song1_name", edit.song1_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song1_name")
+    }
+    else if(value === 'song2'){
+        detailsForm.append('file', songFile2);
+        detailsForm.append("song2_name", edit.song2_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song2_name")
+    }
+    else if(value === 'song3'){
+        detailsForm.append('file', songFile3);
+        detailsForm.append("song3_name", edit.song3_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song3_name")
+    }
+    else if(value === 'song4'){
+        detailsForm.append('file', songFile4);
+        detailsForm.append("song4_name", edit.song4_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song4_name")
+    }
+    else if(value === 'song5'){
+        detailsForm.append('file', songFile5);
+        detailsForm.append("song5_name", edit.song5_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song5_name")
+    }
+    else if(value === 'song6'){
+        detailsForm.append('file', songFile6);
+        detailsForm.append("song6_name", edit.song6_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song6_name")
+    }
+    else if(value === 'song7'){
+        detailsForm.append('file', songFile7);
+        detailsForm.append("song7_name", edit.song7_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song7_name")
+    }
+    else if(value === 'song8'){
+        detailsForm.append('file', songFile8);
+        detailsForm.append("song8_name", edit.song8_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song8_name")
+    }
+    else if(value === 'song9'){
+        detailsForm.append('file', songFile9);
+        detailsForm.append("song9_name", edit.song9_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song9_name")
+    }
+    else if(value === 'song10'){
+        detailsForm.append('file', songFile10);
+        detailsForm.append("song10_name", edit.song10_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song10_name")
+    }
+    else if(value === 'song11'){
+        detailsForm.append('file', songFile11);
+        detailsForm.append("song11_name", edit.song11_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song11_name")
+    }
+    else if(value === 'song12'){
+        detailsForm.append('file', songFile12);
+        detailsForm.append("song12_name", edit.song12_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song12_name")
+    }
+    else if(value === 'song13'){
+        detailsForm.append('file', songFile13);
+        detailsForm.append("song13_name", edit.song13_name)
+        dispatch({
+            type: 'UPDATE_LEARNING_PACK',
+            payload: {id: edit.id, data: detailsForm}
+          })
+        detailsForm.delete('file')
+        detailsForm.delete("song13_name")
+    }
+  }
+
   const submitPack = (event) => {
     event.preventDefault()
-    let files = []
     // confirmation message
     Swal.fire({
       icon: "question",
@@ -121,6 +253,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song1")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 1
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 2 Title
             <input
               value={edit.song2_name}
@@ -137,6 +277,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
                 onChange={(evt) => setSongFile2(evt.target.files[0])}
               ></input>
               </label>
+        </div>
+        <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song2")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 2
+        </Button>
         </div>
         <div className='admin-details-edit-row'>
           <label> Song 3 Title
@@ -157,6 +305,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song3")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 3
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 4 Title
             <input
               value={edit.song4_name}
@@ -173,6 +329,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
                 onChange={(evt) => setSongFile4(evt.target.files[0])}
               ></input>
               </label>
+        </div>
+        <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song4")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 4
+        </Button>
         </div>
         <div className='admin-details-edit-row'>
           <label> Song 5 Title
@@ -193,6 +357,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song5")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 5
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 6 Title
             <input
               value={edit.song6_name}
@@ -209,6 +381,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
                 onChange={(evt) => setSongFile6(evt.target.files[0])}
               ></input>
               </label>
+        </div>
+        <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song6")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 6
+        </Button>
         </div>
         <div className='admin-details-edit-row'>
           <label> Song 7 Title
@@ -229,6 +409,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song7")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 7
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 8 Title
             <input
               value={edit.song8_name}
@@ -245,6 +433,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
                 onChange={(evt) => setSongFile8(evt.target.files[0])}
               ></input>
               </label>
+        </div>
+        <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song8")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 8
+        </Button>
         </div>
         <div className='admin-details-edit-row'>
           <label> Song 9 Title
@@ -265,6 +461,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song9")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 9
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 10 Title
             <input
               value={edit.song10_name}
@@ -281,6 +485,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
                 onChange={(evt) => setSongFile10(evt.target.files[0])}
               ></input>
               </label>
+        </div>
+        <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song10")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 10
+        </Button>
         </div>
         <div className='admin-details-edit-row'>
           <label> Song 11 Title
@@ -301,6 +513,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song11")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 11
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 12 Title
             <input
               value={edit.song12_name}
@@ -319,6 +539,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
               </label>
         </div>
         <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song12")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 12
+        </Button>
+        </div>
+        <div className='admin-details-edit-row'>
           <label> Song 13 Title
             <input
               value={edit.song13_name}
@@ -335,6 +563,14 @@ export default function LearningPackDialog({ setDetailsOpen }) {
                 onChange={(evt) => setSongFile13(evt.target.files[0])}
               ></input>
               </label>
+        </div>
+        <div className='admin-details-edit-row'>
+        <Button variant="contained"
+          onClick={() => uploadButton("song13")}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black", justifySelf: "center" }}
+        >
+          Upload Song 13
+        </Button>
         </div>
         <div className='admin-details-edit-row'>
         <label> Play Guide PDF:
