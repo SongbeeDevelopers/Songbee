496c37eeb6c82058454b20626961dc866f75dd77
Walker Neudorff <walkerneudorff@gmail.com>
2024-06-03 14:07:52 -0500

    Added w9 upload in artist profile

--- src/components/UserPortals/ArtistPortal/ArtistPortalTabs/ArtistDocuments.jsx
+++ src/components/UserPortals/ArtistPortal/ArtistPortalTabs/ArtistDocuments.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TextField, Button } from "@mui/material";
 
 import '../ArtistPortal.css'
 
 function ArtistDocuments() {
+
+    const dispatch = useDispatch();
+    const [file, setFile] = useState('');
+    const artistProfile = useSelector((store) => store.artistProfile);
+    const detailsForm = new FormData()
+
+    const handleUpload = () => {
+        detailsForm.append("file", file);
+        detailsForm.append("artist", artistProfile.id);
+        dispatch({
+            type: "UPDATE_ARTIST_FILE",
+            payload: {id: 5,
+                    data: detailsForm
+            }
+        })
+        setFile('')
+    }
     return (
         <>
         <div className="reqFormGroup">
@@ -22,13 +42,23 @@ function ArtistDocuments() {
         </div>
         <div className="w9-input">
          <label> 
-                Click here to upload your w9
+                Upload Your W9
                 <br></br>
-            <input
-            type="file"
-            >
-            </input>
+            <TextField
+              type="file"
+              className="form-control-file"
+              name="uploaded_file"
+              onChange={(evt) => setFile(evt.target.files[0])}
+              // fullWidth={true}
+              sx={{ width: 300, mt: 3, ml: -6 }}
+            />
          </label>
+         <Button
+          onClick={handleUpload}
+          sx={{m: 'auto', mt: 2, height: 35, width: 200, backgroundColor: "#feaf17", color: "black" }}
+        >
+          UPDATE W9
+        </Button>
         </div>
         </>
         
