1f2fef0e0523fb222b82cba5e53b7b8eefe9ca38
Leo Johnson <leochinonejohnson@gmail.com>
2024-06-03 19:54:30 -0500

    removed artist from jr header

--- src/components/JrHeader/JrHeader.jsx
+++ src/components/JrHeader/JrHeader.jsx
@@ -29,7 +29,6 @@ function JrHeader() {
   const open = Boolean(anchorEl);
 
   const handleMenu = (event) => {
-    console.log('hello')
     setAnchorEl(event.currentTarget);
   };
   const handleClose = (num) => {
@@ -81,11 +80,6 @@ function JrHeader() {
           {!isMobile && user.id && user.class === 3 &&
             <> <Link to="/admin" className="nav-links">Admin</Link> | </>
           }
-
-          {!isMobile && user.id && user.class > 1 &&
-            <> <Link to="/artist" className="nav-links">Artist</Link> | </>
-          }
-
           {!isMobile && user.id &&
             <> <Link to="/user" className="nav-links">Account</Link> | </>
           }
