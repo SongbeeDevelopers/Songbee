ab81259678894a3707fa796a0b43d04c46f36839
Leo Johnson <leochinonejohnson@gmail.com>
2024-06-05 13:18:45 -0500

    added new column to song reqs without breaking anything hopefully

--- server/routes/request.router.js
+++ server/routes/request.router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const {
-    rejectUnauthenticated,
-  } = require("../modules/authentication-middleware");
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 /**
  * GET route template
  */
@@ -31,6 +31,7 @@ router.get('/user', rejectUnauthenticated, (req, res) => {
   "song_request"."created_at",
   "song_request"."delivery_days",
   "song_request"."is_complete",
+  "song_request"."is_approved",
   "song_request"."streaming",
   "song_request"."backing_track",
   "song_request"."license",
@@ -51,19 +52,19 @@ router.get('/user', rejectUnauthenticated, (req, res) => {
   WHERE "song_request"."user_id"=$1;
   `
   pool.query(requestQuery, [userId])
-  .then((result) => {
-    res.send(result.rows);
-    // console.log("Request router GET all user requests", result.rows)
-  })
-  .catch((error) => {
-    console.error("Error in request router GET all user requests", error);
-    res.sendStatus(500);
-  })
+    .then((result) => {
+      res.send(result.rows);
+      // console.log("Request router GET all user requests", result.rows)
+    })
+    .catch((error) => {
+      console.error("Error in request router GET all user requests", error);
+      res.sendStatus(500);
+    })
 });
 
 router.get('/all', rejectUnauthenticated, async (req, res) => {
-    let connection
-    try {
+  let connection
+  try {
     connection = await pool.connect();
     connection.query("BEGIN;");
     const pendingRequestQuery = `
@@ -87,6 +88,7 @@ router.get('/all', rejectUnauthenticated, async (req, res) => {
     "song_request"."created_at",
     "song_request"."delivery_days",
     "song_request"."is_complete",
+    "song_request"."is_approved",
     "song_request"."streaming",
     "song_request"."backing_track",
     "song_request"."license",
@@ -110,6 +112,7 @@ router.get('/all', rejectUnauthenticated, async (req, res) => {
     WHERE "song_request"."is_complete"=FALSE;
     `
     const pendingResult = await connection.query(pendingRequestQuery);
+
     const completedRequestQuery = `
     SELECT 
     "song_request"."id" AS "id",
@@ -131,6 +134,7 @@ router.get('/all', rejectUnauthenticated, async (req, res) => {
     "song_request"."created_at",
     "song_request"."delivery_days",
     "song_request"."is_complete",
+    "song_request"."is_approved",
     "song_request"."streaming",
     "song_request"."backing_track",
     "song_request"."license",
@@ -154,19 +158,21 @@ router.get('/all', rejectUnauthenticated, async (req, res) => {
     WHERE "song_request"."is_complete"=TRUE;
     `
     const completedResult = await connection.query(completedRequestQuery);
+
     connection.query("COMMIT;");
     connection.release();
     res.send([pendingResult.rows, completedResult.rows])
-    } catch (error) {
-        console.log("Error in request router GET all:", error);
-        connection.query("ROLLBACK;");
-        connection.release();
-        res.sendStatus(500);
-    }
-  });
+
+  } catch (error) {
+    console.log("Error in request router GET all:", error);
+    connection.query("ROLLBACK;");
+    connection.release();
+    res.sendStatus(500);
+  }
+});
 
 router.get('/current/:id', (req, res) => {
-    const query = `
+  const query = `
     SELECT 
     "song_request"."id" AS "id",
     "song_request"."user_id",
@@ -187,6 +193,7 @@ router.get('/current/:id', (req, res) => {
     "song_request"."created_at",
     "song_request"."delivery_days",
     "song_request"."is_complete",
+    "song_request"."is_approved",
     "song_request"."streaming",
     "song_request"."backing_track",
     "song_request"."license",
@@ -211,22 +218,18 @@ router.get('/current/:id', (req, res) => {
     ON "song_details"."artist_id"="artist"."id"
     WHERE "song_request"."id"=$1;
     `
-    pool.query(query, [req.params.id])
+  pool.query(query, [req.params.id])
     .then((result) => {
-        res.send(result.rows);
+      res.send(result.rows);
     })
     .catch((error) => {
-        console.error("Error in request router GET current request:", error)
+      console.error("Error in request router GET current request:", error)
     })
 });
 
-/**
- * POST route template
- */
-
 router.post('/create', async (req, res) => {
-    let connection
-    try {
+  let connection
+  try {
     connection = await pool.connect();
 
     connection.query("BEGIN;");
@@ -254,19 +257,34 @@ router.post('/create', async (req, res) => {
       ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
       RETURNING "id";
     `
-    const response = await connection.query(requestQuery, [userId, genreId, requester, recipient, pronunciation, recipientRelationship, occasion, vocalType, vibe, tempo, inspiration, deliveryDays, streaming, extraVerse, license, backingTrack])
-
-    // console.log("artist:", artist);
-    // console.log("req.body:", req.body)
-    if (artist === ''){
-    const detailsQuery = `
+    const response = await connection.query(
+      requestQuery, [
+        userId, 
+        genreId, 
+        requester, 
+        recipient, 
+        pronunciation, 
+        recipientRelationship, 
+        occasion, 
+        vocalType, 
+        vibe, 
+        tempo, 
+        inspiration, 
+        deliveryDays, 
+        streaming, 
+        extraVerse, 
+        license, 
+        backingTrack
+      ]
+    )
+    if (artist === '') {
+      const detailsQuery = `
     INSERT INTO "song_details"
         ("song_request_id")
         VALUES
         ($1)
     `
-    // console.log('created id:', response.rows[0].id)
-    const detailsResponse = await connection.query(detailsQuery, [response.rows[0].id])
+      const detailsResponse = await connection.query(detailsQuery, [response.rows[0].id])
     } else {
       const detailsQuery = `
       INSERT INTO "song_details"
@@ -274,33 +292,31 @@ router.post('/create', async (req, res) => {
           VALUES
           ($1, $2)
       `
-      // console.log('created id:', response.rows[0].id)
       const detailsResponse = await connection.query(detailsQuery, [response.rows[0].id, artist])
     }
     connection.query("COMMIT;");
     connection.release();
-    res.send({id: response.rows[0].id})
-    } catch (error) {
-        console.error("Error in request router POST create request", error);
-        connection.query("ROLLBACK;");
-        connection.release();
-        res.sendStatus(500);
-    }
-  });
+    res.send({ id: response.rows[0].id })
+  } catch (error) {
+    console.error("Error in request router POST create request", error);
+    connection.query("ROLLBACK;");
+    connection.release();
+    res.sendStatus(500);
+  }
+});
 
 router.put('/finish/:id', async (req, res) => {
-  // console.log("req.body:", req.body)
-    let connection
-    try {
+  let connection
+  try {
     connection = await pool.connect();
-
     connection.query("BEGIN;");
+
     const story1 = req.body.story1
     const story2 = req.body.story2
     const important_what = req.body.important_what
     const important_why = req.body.important_why
     const additional_info = req.body.additional_info
-    
+
     const requestQuery = `
     UPDATE "song_request"
     SET
@@ -311,88 +327,110 @@ router.put('/finish/:id', async (req, res) => {
     "additional_info"=$5
     WHERE "id"=$6
     `
-    const response = await connection.query(requestQuery, [story1, story2, important_what, important_why, additional_info, req.params.id])
-
+    const response = await connection.query(
+      requestQuery, [
+        story1, 
+        story2, 
+        important_what, 
+        important_why, 
+        additional_info, 
+        req.params.id
+      ]
+    )
     connection.query("COMMIT;");
     connection.release();
     res.sendStatus(201);
-    } catch (error) {
-        console.error("Error in request router PUT final questions", error);
-        connection.query("ROLLBACK;");
-        connection.release();
-        res.sendStatus(500);
-    }
-  });
+  } catch (error) {
+    console.error("Error in request router PUT final questions", error);
+    connection.query("ROLLBACK;");
+    connection.release();
+    res.sendStatus(500);
+  }
+});
 
 router.put('/update/:id', rejectUnauthenticated, async (req, res) => {
-try {
-  const requester = req.body.requester;
-  const recipient = req.body.recipient;
-  const pronunciation = req.body.pronunciation;
-  const recipientRelationship = req.body.recipient_relationship;
-  const occasion = req.body.occasion;
-  const genreId = req.body.genre;
-  const vocalType = req.body.vocal_type;
-  const vibe = req.body.vibe;
-  const tempo = req.body.tempo;
-  const inspiration = req.body.inspiration;
-  const story1 = req.body.story1;
-  const story2 = req.body.story2;
-  const importantWhat = req.body.important_what;
-  const importantWhy = req.body.important_why;
-  const additionalInfo = req.body.additional_info;
-  const requestId = req.params.id
+  try {
+    const requester = req.body.requester;
+    const recipient = req.body.recipient;
+    const pronunciation = req.body.pronunciation;
+    const recipientRelationship = req.body.recipient_relationship;
+    const occasion = req.body.occasion;
+    const genreId = req.body.genre;
+    const vocalType = req.body.vocal_type;
+    const vibe = req.body.vibe;
+    const tempo = req.body.tempo;
+    const inspiration = req.body.inspiration;
+    const story1 = req.body.story1;
+    const story2 = req.body.story2;
+    const importantWhat = req.body.important_what;
+    const importantWhy = req.body.important_why;
+    const additionalInfo = req.body.additional_info;
+    const requestId = req.params.id
 
-  const requestQuery = `
-  UPDATE "song_request"
-  SET
-    "requester"=$1, 
-    "recipient"=$2, 
-    "pronunciation"=$3, 
-    "recipient_relationship"=$4, 
-    "occasion"=$5, 
-    "genre_id"=$6, 
-    "vocal_type"=$7, 
-    "vibe"=$8, 
-    "tempo"=$9, 
-    "inspiration"=$10, 
-    "story1"=$11, 
-    "story2"=$12, 
-    "important_what"=$13, 
-    "important_why"=$14, 
-    "additional_info"=$15
-  WHERE "id"=$16
-    ;
-  `
-  const requestValues = [
-    requester, recipient, pronunciation, recipientRelationship, occasion, genreId, vocalType, vibe, tempo, inspiration, story1, story2, importantWhat, importantWhy, additionalInfo, requestId
-  ]
-  const requestResult = await pool.query(requestQuery, requestValues);
-  res.sendStatus(201);
-} catch (error) {
+    const requestQuery = `
+    UPDATE "song_request"
+    SET
+      "requester"=$1, 
+      "recipient"=$2, 
+      "pronunciation"=$3, 
+      "recipient_relationship"=$4, 
+      "occasion"=$5, 
+      "genre_id"=$6, 
+      "vocal_type"=$7, 
+      "vibe"=$8, 
+      "tempo"=$9, 
+      "inspiration"=$10, 
+      "story1"=$11, 
+      "story2"=$12, 
+      "important_what"=$13, 
+      "important_why"=$14, 
+      "additional_info"=$15
+    WHERE "id"=$16;
+    `
+    const requestValues = [
+      requester, 
+      recipient, 
+      pronunciation, 
+      recipientRelationship, 
+      occasion, 
+      genreId, 
+      vocalType, 
+      vibe, 
+      tempo, 
+      inspiration, 
+      story1, 
+      story2, 
+      importantWhat, 
+      importantWhy, 
+      additionalInfo, 
+      requestId
+    ]
+    const requestResult = await pool.query(requestQuery, requestValues);
+    res.sendStatus(201);
+  } catch (error) {
     console.error("Error in request router POST", error)
-}
+  }
 });
 
 router.delete("/:id", rejectUnauthenticated, (req, res) => {
-    const requestId = req.params.id;
-    const query = `
-          DELETE FROM "song_request"
-            WHERE "id"=$1;
-      `;
-    pool
-      .query(query, [requestId])
-      .then((result) => {
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        console.log("Error in request router DELETE request", err);
-        res.sendStatus(500);
-      });
-  });
+  const requestId = req.params.id;
+  const query = `
+    DELETE FROM "song_request"
+    WHERE "id"=$1;
+  `;
+  pool
+    .query(query, [requestId])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("Error in request router DELETE request", err);
+      res.sendStatus(500);
+    });
+});
 
-  router.get('/artist/:id', rejectUnauthenticated, (req, res) => {
-    const requestQuery = `
+router.get('/artist/:id', rejectUnauthenticated, (req, res) => {
+  const requestQuery = `
     SELECT 
     "song_request"."id" AS "id",
     "song_request"."user_id",
@@ -413,6 +451,7 @@ router.delete("/:id", rejectUnauthenticated, (req, res) => {
     "song_request"."created_at",
     "song_request"."delivery_days",
     "song_request"."is_complete",
+    "song_request"."is_approved",
     "song_details"."url",
     "song_details"."lyrics",
     "song_details"."title",
@@ -433,7 +472,7 @@ router.delete("/:id", rejectUnauthenticated, (req, res) => {
     AND
     "song_request"."is_complete"=FALSE;
     `
-    pool.query(requestQuery, [req.params.id])
+  pool.query(requestQuery, [req.params.id])
     .then((result) => {
       res.send(result.rows);
       // console.log("Request router GET all user requests", result.rows)
@@ -442,10 +481,10 @@ router.delete("/:id", rejectUnauthenticated, (req, res) => {
       console.error("Error in request router GET all artist requests", error);
       res.sendStatus(500);
     })
-  });
+});
 
-  router.get('/artist/complete/:id', rejectUnauthenticated, (req, res) => {
-    const requestQuery = `
+router.get('/artist/complete/:id', rejectUnauthenticated, (req, res) => {
+  const requestQuery = `
     SELECT 
     "song_request"."id" AS "id",
     "song_request"."user_id",
@@ -466,6 +505,7 @@ router.delete("/:id", rejectUnauthenticated, (req, res) => {
     "song_request"."created_at",
     "song_request"."delivery_days",
     "song_request"."is_complete",
+    "song_request"."is_approved",
     "song_details"."url",
     "song_details"."lyrics",
     "song_details"."title",
@@ -487,7 +527,7 @@ router.delete("/:id", rejectUnauthenticated, (req, res) => {
     AND
     "song_request"."is_complete"=TRUE;
     `
-    pool.query(requestQuery, [req.params.id])
+  pool.query(requestQuery, [req.params.id])
     .then((result) => {
       res.send(result.rows);
       // console.log("Request router GET all user requests", result.rows)
@@ -496,30 +536,30 @@ router.delete("/:id", rejectUnauthenticated, (req, res) => {
       console.error("Error in request router GET all artist requests", error);
       res.sendStatus(500);
     })
-  });
+});
 
-  router.put('/confirm/:id', async (req, res) => {
-    let connection
-    try {
-      connection = await pool.connect();
-  
-      connection.query("BEGIN;");
-      const updateQuery = `
+router.put('/confirm/:id', async (req, res) => {
+  let connection
+  try {
+    connection = await pool.connect();
+
+    connection.query("BEGIN;");
+    const updateQuery = `
     UPDATE "song_request"
     SET 
       "is_paid" = TRUE
     WHERE id=$1;
     `
-      await connection.query(updateQuery, [req.params.id])
-      connection.query("COMMIT;");
-      connection.release();
-      res.sendStatus(200);
-    } catch (error) {
-      console.error("Request router confirm payment failed:", error)
-      connection.query("ROLLBACK;");
-      connection.release();
-      res.sendStatus(500)
-    }
-  })
+    await connection.query(updateQuery, [req.params.id])
+    connection.query("COMMIT;");
+    connection.release();
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Request router confirm payment failed:", error)
+    connection.query("ROLLBACK;");
+    connection.release();
+    res.sendStatus(500)
+  }
+})
 
 module.exports = router;
