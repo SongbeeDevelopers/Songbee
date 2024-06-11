1fd00e421db8f50459945f273c64c35b12571912
Leo Johnson <leochinonejohnson@gmail.com>
2024-06-11 13:39:29 -0500

    replaced video on pack views

--- src/components/LearningPackView/PacksVideo.jsx
+++ src/components/LearningPackView/PacksVideo.jsx
@@ -7,13 +7,14 @@ function LearningPackVideo() {
       <img className="video-colorMusicNotes" src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714076545/Songbee/color-music-notes_ouootu.png" alt="" />
       <img className="video-blackMusicNotes" src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714076604/Songbee/music-notes-black_hvqvax.png" alt="" />
       <img className="video-colorFlowers" src="https://res.cloudinary.com/dke4ukd0z/image/upload/v1714076551/Songbee/colorflowers_wesazl.png" alt="" />
-      <video
-        src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      <iframe
+        style={{width:'100vw',height:'calc(1600/900*100vw)',border:'none', padding: '25px'}}
+        src="https://drive.google.com/file/d/1OxSXi_Oa2PAV8IBjLJsQ9CINUUL87xfv/preview?"
         autoPlay
         muted
         loop
         controls
-      ></video>
+      ></iframe>
       <h2>Play To Learn!</h2>
     </div>
   );
