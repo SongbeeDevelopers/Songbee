5db07e70b7ad8226fd701793f09919aa3669922f
Leo Johnson <leochinonejohnson@gmail.com>
2024-06-14 11:02:05 -0500

    finished schools route

--- server/routes/schools.router.js
+++ server/routes/schools.router.js
@@ -3,7 +3,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   const query = `
     SELECT * from "schools";
   `
@@ -17,7 +17,61 @@ router.get('/', (req, res) => {
 });
 
 router.put('/', rejectUnauthenticated, (req, res) => {
+  const query = `
+  UPDATE "schools"
+  SET
+    "contact" = $1,
+    "institution" = $2,
+    "license_length" = $3,
+    "agreement" = $4
+  WHERE "id" = $5
+  `
+  pool.query(query, [
+    req.body.contact,
+    req.body.institution,
+    req.body.license_length,
+    req.body.agreement,
+    req.body.id
+  ])
+  .then((result) => {
+    res.sendStatus(200)
+  })
+  .catch((error) => {
+    res.sendStatus(500)
+    console.error('schools put router failed:', error)
+  })
+})
 
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const query = `
+  INSERT INTO "schools" ("contact", "institution", "license_length", "agreement")
+  VALUES ($1, $2, $3, $4)
+  `
+  pool.query(query, [
+    req.body.contact,
+    req.body.institution,
+    req.body.license_length,
+    req.body.agreement
+  ])
+  .then((result) => {
+    res.sendStatus(201)
+  })
+  .catch((error) => {
+    res.sendStatus(500)
+  })
+})
+
+router.delete('/', rejectUnauthenticated, (req, res) => {
+  const query = `
+  DELETE FROM "schools" WHERE "id" = $1
+  `
+  pool.query(query, [req.body.id])
+  .then((result) => {
+    res.sendStatus(200)
+  })
+  .catch((error) => {
+    res.sendStatus(500)
+  })
 })
 
 module.exports = router;
--- src/redux/sagas/schools.saga.js
+++ src/redux/sagas/schools.saga.js
@@ -13,8 +13,50 @@ function* fetchSchoolData (action) {
     }
 }
 
+function* updateSchoolData (action) {
+    try {
+        yield axios({
+            method: 'PUT',
+            url: '/api/schools',
+            data: action.payload
+        })
+        yield put({type: 'FETCH_SCHOOL_DATA'})
+    } catch (error) {
+        console.error('saga updateSchoolData() failed:', error)
+    }
+}
+
+function* addSchoolData (action) {
+    try {
+        yield axios({
+            method: 'POST',
+            url: '/api/schools',
+            data: action.payload
+        })
+        yield put({type: 'FETCH_SCHOOL_DATA'})
+    } catch (error) {
+        console.error('saga addSchoolData failed:', error) 
+    }
+}
+
+function* deleteSchoolData (action) {
+    try {
+        yield axios({
+            method: 'DELETE',
+            url: '/api/schools',
+            data: action.payload
+        })
+        yield put({type: 'FETCH_SCHOOL_DATA'})
+    } catch (error) {
+        console.error('saga deleteSchoolData failed:', error)
+    }
+}
+
 function* requestSaga() {
     yield takeLatest('FETCH_SCHOOL_DATA', fetchSchoolData);
+    yield takeLatest('UPDATE_SCHOOL_DATA', updateSchoolData);
+    yield takeLatest('ADD_SCHOOL_DATA', addSchoolData);
+    yield takeLatest('DELETE_SCHOOL_DATA', deleteSchoolData);
 }
 
 export default requestSaga;
