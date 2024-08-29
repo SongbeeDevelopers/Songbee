41e13577941a1f9cf2dbfd6439a20eac27d1b8b5
Walker Neudorff <walkerneudorff@gmail.com>
2024-08-29 13:19:11 -0500

    Adding new button to customer portal

--- src/components/UserPortals/CustomerPortal/CustomerPortalTabs/CustomerRequestsTab.jsx
+++ src/components/UserPortals/CustomerPortal/CustomerPortalTabs/CustomerRequestsTab.jsx
@@ -49,9 +49,29 @@ function CustomerRequests() {
   const startSong = () => {
     history.push('/order')
   }
+  const completeRequest = (reqId) => {
+    history.push(`/finalquestions/${reqId}`)
+  }
 
-  const statusDisplay = (req) => {
-    
+  const StatusDisplay = ({req}) => {
+    if (!req.customer_complete){
+      return (
+        <>
+          <Button variant="contained"
+            onClick={() => completeRequest(req.id)}
+            sx={{ height: 35, width: 180, backgroundColor: "#feaf17", color: "black" }}
+          >
+            Finish Order
+          </Button>
+        </>
+      )
+    } else if (!req.accepted){
+      return `Pending Artist Acceptance`
+    } else if (req.accepted && !req.is_complete){
+      return `In Progress`
+    } else if (req.is_complete){
+      return `Complete!`
+    }
   }
 
   function getDueDate(requestDay, deliveryDays) {
@@ -104,7 +124,7 @@ function CustomerRequests() {
 
                 {/* status */}
                 <TableCell align="center">
-                  {request.accepted ? request.isComplete ? `In Progress` : `Complete!` : `Pending Artist Acceptance`}
+                  <StatusDisplay req={request} />
                 </TableCell>
 
                 {/* details */}
